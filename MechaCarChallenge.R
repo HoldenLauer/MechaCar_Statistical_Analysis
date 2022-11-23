33f2d95963c4582b183ba3d8ab19f1abed02d4c9
Holden Lauer <holdenlauer@gmail.com>
2022-11-22 21:34:49 -0600

    adding D2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,17 @@ Mecha <- read.csv("MechaCar_mpg.csv")
 reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mecha)
 
 summary(reg)
+
+### Deliverable 2
+
+Suspension <- read.csv("Suspension_Coil.csv")
+
+total_summary <- Suspension %>% summarize(Mean_PSI=mean(PSI),
+                           Median_PSI=median(PSI),
+                           Var_PSI=var(PSI),
+                           Std_Dev_PSI=sd(PSI))
+
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+                                                                        Median_PSI=median(PSI),
+                                                                        Var_PSI=var(PSI),
+                                                                        Std_Dev_PSI=sd(PSI))
